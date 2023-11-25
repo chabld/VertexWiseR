a1bbbf7f6aa71fc63ceeaed8bb3b01aec364c145
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2023-11-26 01:31:49 +0800

    Update vertwise.r

--- vertwise.r
+++ vertwise.r
@@ -114,8 +114,10 @@ vertex_analysis=function(all_predictors,IV_of_interest, CT_data, p=0.05, atlas=1
       clus_tstat[neg_clusterIDmap!=clusno]=0
       cluster_neg$tstat[clusno]=round(clus_tstat[which.min(clus_tstat)],2)
       cluster_neg[clusno,4:6]=round(model$coord[,which.max(abs(clus_tstat))],1)
- 
-      load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/ROImap.rdata?raw=TRUE"))
+      if(exists("ROImap", inherit=F)==F)
+        {
+        load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/ROImap.rdata?raw=TRUE"))
+        } 
       idx_neg=ROImap[[1]][,atlas][which.min(clus_tstat)]
       cluster_neg$region[clusno]=ROImap[[2]][,atlas][idx_neg]
 
