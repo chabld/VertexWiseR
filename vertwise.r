74921cb9700351c0dd5dbc0a0a70a5e6164a0af2
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2023-12-19 16:46:57 +0800

    Update vertwise.r

--- vertwise.r
+++ vertwise.r
@@ -32,7 +32,7 @@ fs5_to_atlas=function(data,atlas) ## atlas: 1=Desikan, 2=Schaefer-100, 3=Schaefe
   return(ROI)
 }
 
-atlas_to_fs5=function(data,atlas) ## atlas: 1=Desikan, 2=Schaefer-100, 3=Schaefer-200, 4=Glasser-360
+atlas_to_fs5=function(data,atlas) ## atlas: 1=Desikan, 2=Schaefer-100, 3=Schaefer-200, 4=Glasser-360, 5=Destrieux-148
   {
   load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/ROImap.rdata?raw=TRUE"))
   nregions=max(ROImap[[1]][,atlas])
