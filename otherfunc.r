33fa4aa6775f34434aca558f5ce00f9bb5e92dec
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-02 17:31:37 +0800

    Update otherfunc.r

--- otherfunc.r
+++ otherfunc.r
@@ -275,7 +275,7 @@ decode_img=function(img,contrast="positive")
   if(file.exists("neurosynth_dataset.pkl.gz")==F)
   {
     cat("\nneurosynth_dataset.pkl.gz is not detected in the current working directory. The neurosynth database will be downloaded\n")
-    download.file(url="https://blogs.ntu.edu.sg/cogbrainhealthlab/files/2023/10/neurosynth_dataset.pkl_.gz",destfile = "neurosynth_dataset.pkl.gz")
+    download.file(url="https://raw.githubusercontent.com/CogBrainHealthLab/VertexWiseR/main/data/neurosynth_dataset.pkl.gz",destfile = "neurosynth_dataset.pkl.gz")
   } 
   ##running the decoding procedure
   neurosynth_dset = nimare.dataset$Dataset$load("neurosynth_dataset.pkl.gz")
