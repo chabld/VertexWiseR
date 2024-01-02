b32a40570081d0c704d1255bd998b130831ccd2b
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-02 15:50:11 +0800

    Update vertTFCE.r

--- vertTFCE.r
+++ vertTFCE.r
@@ -35,7 +35,7 @@ TFCE.vertex_analysis=function(all_predictors,IV_of_interest, CT_data, nperm=100,
       {
         if(colno==(NCOL(all_predictors)+1))  {stop("IV_of_interest is not contained within all_predictors")}
         
-        if(class(all_predictors[,column]) != "integer" & class(all_predictors[,column]) != "numeric")  {if(identical(IV_of_interest,all_predictors[,colno]))  {break}} 
+        if(class(all_predictors[,colno]) != "integer" & class(all_predictors[,colno]) != "numeric")  {if(identical(IV_of_interest,all_predictors[,colno]))  {break}} 
         else  {if(identical(as.numeric(IV_of_interest),as.numeric(all_predictors[,colno])))  {break}}
       }
   
