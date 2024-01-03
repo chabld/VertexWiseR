06828c7c35ed271772ecf8bdd08e4390d997c18b
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-03 12:38:28 +0800

    Update vertwise_mixed.r

--- vertwise_mixed.r
+++ vertwise_mixed.r
@@ -8,88 +8,90 @@
 vertex_analysis.mixed=function(all_predictors,IV_of_interest, random_effect, CT_data, p=0.05, atlas=1)  ## atlas: 1=Desikan, 2=Schaefer-100, 3=Schaefer-200, 4=Glasser-360, 5=Destrieux-148
 {
   ##checks
-  # check required packages
-  list.of.packages ="reticulate"
-  new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) 
-  {
-    cat(paste("The following package(s) are required and will be installed:\n",new.packages,"\n"))
-    install.packages(new.packages)
-  }
-  #check if nrow is consistent for all_predictors and FC_data
-  if(NROW(CT_data)!=NROW(all_predictors))  {stop(paste("The number of rows for CT_data (",NROW(CT_data),") and all_predictors (",NROW(all_predictors),") are not the same",sep=""))}
-  
-  #incomplete data check
-  idxF=which(complete.cases(all_predictors)==F)
-  if(length(idxF)>0)
-  {
-    cat(paste("all_predictors contains",length(idxF),"subjects with incomplete data. Subjects with incomplete data will be excluded in the current analysis"))
-    all_predictors=all_predictors[-idxF,]
-    IV_of_interest=IV_of_interest[-idxF]
-    CT_data=CT_data[-idxF,]
-  }
-  
-  #check IV_of_interest
-  for(colno in 1:(NCOL(all_predictors)+1))
-  {
-    if(colno==(NCOL(all_predictors)+1))  {stop("IV_of_interest is not contained within all_predictors")}
-    
-    if(class(IV_of_interest) != "integer" & class(IV_of_interest) != "numeric") 
+    #check required packages
+    list.of.packages ="reticulate"
+    new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+    if(length(new.packages)) 
     {
-      if(identical(IV_of_interest,all_predictors[,colno]))  {break} 
-    } else 
-    {
-      if(identical(as.numeric(IV_of_interest),as.numeric(all_predictors[,colno])))  {break}
+      cat(paste("The following package(s) are required and will be installed:\n",new.packages,"\n"))
+      install.packages(new.packages)
     }
-  }
-  #check random predictor
-  for(ran_colno in 1:(NCOL(all_predictors)+1))
-  {
-    if(ran_colno==(NCOL(all_predictors)+1))  {stop("random_effect is not contained within all_predictors")}
+    #check if nrow is consistent for all_predictors and FC_data
+    if(NROW(CT_data)!=NROW(all_predictors))  {stop(paste("The number of rows for CT_data (",NROW(CT_data),") and all_predictors (",NROW(all_predictors),") are not the same",sep=""))}
     
-    if(class(random_effect) != "integer" & class(random_effect) != "numeric") 
+    #incomplete data check
+    idxF=which(complete.cases(all_predictors)==F)
+    if(length(idxF)>0)
     {
-      if(identical(random_effect,all_predictors[,ran_colno]))  {break} 
-    } else 
-    {
-      if(identical(as.numeric(random_effect),as.numeric(all_predictors[,ran_colno])))  {break}
+      cat(paste("all_predictors contains",length(idxF),"subjects with incomplete data. Subjects with incomplete data will be excluded in the current analysis"))
+      all_predictors=all_predictors[-idxF,]
+      IV_of_interest=IV_of_interest[-idxF]
+      CT_data=CT_data[-idxF,]
     }
-  }
-  
-  #check categorical variable
-  for (column in 1:NCOL(all_predictors))
-  {
-    if(column!=ran_colno) #skip check for random effect
+    
+    #check random predictor
+    if(!missing("random_effect"))
       {
-       if(class(all_predictors[,column]) != "integer" & class(all_predictors[,column]) != "numeric")
+      for(ran_colno in 1:(NCOL(all_predictors)+1))
+        {
+        if(ran_colno==(NCOL(all_predictors)+1))  {stop("random_effect is not contained within all_predictors")}
+        
+        if(class(random_effect) != "integer" & class(random_effect) != "numeric") 
           {
-            if(length(unique(all_predictors[,column]))==2)
-            {
-              cat(paste("The binary variable '",colnames(all_predictors)[column],"' will be recoded with ",unique(all_predictors[,column])[1],"=0 and ",unique(all_predictors[,column])[2],"=1 for the analysis\n",sep=""))
-              
-              recode=rep(0,NROW(all_predictors))
-              recode[all_predictors[,column]==unique(all_predictors[,column])[2]]=1
-              all_predictors[,column]=recode
-              IV_of_interest=all_predictors[,colno]
-            } else if(length(unique(all_predictors[,column]))>2)    {cat(paste("The categorical variable '",colnames(all_predictors)[column],"' contains more than 2 levels, please code it into binarized dummy variables",sep=""))}
-          }      
-      } 
-  }
+            if(identical(random_effect,all_predictors[,ran_colno]))  {break} 
+          } else 
+          {
+            if(identical(as.numeric(random_effect),as.numeric(all_predictors[,ran_colno])))  {break}
+          }
+        }
+      fix_predictors=all_predictors[,-ran_colno]
+      } else {fix_predictors=all_predictors}
   
-  #check length of CT data and smoothing
-  n_vert=ncol(CT_data)
-  if(n_vert==20484) 
-  {    
-    template="fsaverage5"
-    load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/ROImap_fs5.rdata?raw=TRUE"))
-  }
-  else if (n_vert==81924) 
-  {
-    template="fsaverage6"
-    load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/ROImap_fs6.rdata?raw=TRUE"))
-  } 
-  else {stop("CT_data should only contain 20484 (fsaverage5) or 81924 (fsaverage6) columns")}
+    #check IV_of_interest
+    for(colno in 1:(NCOL(fix_predictors)+1))
+    {
+      if(colno==(NCOL(fix_predictors)+1))  {stop("IV_of_interest is not contained within all_predictors")}
+      
+      if(class(IV_of_interest) != "integer" & class(IV_of_interest) != "numeric") 
+      {
+        if(identical(IV_of_interest,fix_predictors[,colno]))  {break} 
+      } else 
+      {
+        if(identical(as.numeric(IV_of_interest),as.numeric(fix_predictors[,colno])))  {break}
+      }
+    }
+  
+    #check categorical variable
+    for (column in 1:NCOL(fix_predictors))
+    {
+      if(class(fix_predictors[,column]) != "integer" & class(fix_predictors[,column]) != "numeric")
+      {
+        if(length(unique(fix_predictors[,column]))==2)
+        {
+          cat(paste("The binary variable '",colnames(fix_predictors)[column],"' will be recoded with ",unique(fix_predictors[,column])[1],"=0 and ",unique(fix_predictors[,column])[2],"=1 for the analysis\n",sep=""))
+            
+          recode=rep(0,NROW(fix_predictors))
+          recode[fix_predictors[,column]==unique(fix_predictors[,column])[2]]=1
+          fix_predictors[,column]=recode
+          IV_of_interest=fix_predictors[,colno]
+        } else if(length(unique(fix_predictors[,column]))>2)    {cat(paste("The categorical variable '",colnames(fix_predictors)[column],"' contains more than 2 levels, please code it into binarized dummy variables",sep=""))}
+      }      
+    }
     
+    #check length of CT data and smoothing
+    n_vert=ncol(CT_data)
+    if(n_vert==20484) 
+    {    
+      template="fsaverage5"
+      load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/ROImap_fs5.rdata?raw=TRUE"))
+    }
+    else if (n_vert==81924) 
+    {
+      template="fsaverage6"
+      load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/ROImap_fs6.rdata?raw=TRUE"))
+    } 
+    else {stop("CT_data should only contain 20484 (fsaverage5) or 81924 (fsaverage6) columns")}
+  
   ##import python libaries
   brainstat.stats.terms=reticulate::import("brainstat.stats.terms")
   brainstat.stats.SLM=reticulate::import("brainstat.stats.SLM")
@@ -102,7 +104,8 @@ vertex_analysis.mixed=function(all_predictors,IV_of_interest, random_effect, CT_
   mask[which(colSums(CT_data != 0) == 0)]=F
   
   #fit model
-  model0=brainstat.stats.terms$MixedEffect(ran = random_effect,fix = all_predictors[,-ran_colno],"_check_categorical" = F)
+  if(missing("random_effect")) {model0=brainstat.stats.terms$FixedEffect(fix_predictors, "_check_categorical" = F)}
+  else {model0=brainstat.stats.terms$MixedEffect(ran = random_effect,fix = fix_predictors,"_check_categorical" = F)}
   model=brainstat.stats.SLM$SLM(model = model0,
                                 contrast=IV_of_interest,
                                 surf = template, 
