17011b2ece64104fed4369183986f7736b58315f
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-05 17:36:26 +0800

    Update vertwise.r

--- vertwise.r
+++ vertwise.r
@@ -7,91 +7,9 @@
 ##vertex wise analysis with mixed effects
 vertex_analysis=function(all_predictors,IV_of_interest, random_effect, CT_data, p=0.05, atlas=1)  ## atlas: 1=Desikan, 2=Schaefer-100, 3=Schaefer-200, 4=Glasser-360, 5=Destrieux-148
 {
-  ##checks
-    #check required packages
-    list.of.packages ="reticulate"
-    new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-    if(length(new.packages)) 
-    {
-      cat(paste("The following package(s) are required and will be installed:\n",new.packages,"\n"))
-      install.packages(new.packages)
-    }
-    #check if nrow is consistent for all_predictors and FC_data
-    if(NROW(CT_data)!=NROW(all_predictors))  {stop(paste("The number of rows for CT_data (",NROW(CT_data),") and all_predictors (",NROW(all_predictors),") are not the same",sep=""))}
-    
-    #incomplete data check
-    idxF=which(complete.cases(all_predictors)==F)
-    if(length(idxF)>0)
-    {
-      cat(paste("all_predictors contains",length(idxF),"subjects with incomplete data. Subjects with incomplete data will be excluded in the current analysis"))
-      all_predictors=all_predictors[-idxF,]
-      IV_of_interest=IV_of_interest[-idxF]
-      CT_data=CT_data[-idxF,]
-    }
-    
-    #check random predictor
-    if(!missing("random_effect"))
-      {
-      for(ran_colno in 1:(NCOL(all_predictors)+1))
-        {
-        if(ran_colno==(NCOL(all_predictors)+1))  {stop("random_effect is not contained within all_predictors")}
-        
-        if(class(random_effect) != "integer" & class(random_effect) != "numeric") 
-          {
-            if(identical(random_effect,all_predictors[,ran_colno]))  {break} 
-          } else 
-          {
-            if(identical(as.numeric(random_effect),as.numeric(all_predictors[,ran_colno])))  {break}
-          }
-        }
-      fix_predictors=all_predictors[,-ran_colno]
-      } else {fix_predictors=all_predictors}
+  localenv = environment() 
+  check.inputs(packages = "reticulate", CT_data = CT_data,all_predictors = all_predictors,IV_of_interest = IV_of_interest)
   
-    #check IV_of_interest
-    for(colno in 1:(NCOL(fix_predictors)+1))
-    {
-      if(colno==(NCOL(fix_predictors)+1))  {stop("IV_of_interest is not contained within all_predictors")}
-      
-      if(class(IV_of_interest) != "integer" & class(IV_of_interest) != "numeric") 
-      {
-        if(identical(IV_of_interest,fix_predictors[,colno]))  {break} 
-      } else 
-      {
-        if(identical(as.numeric(IV_of_interest),as.numeric(fix_predictors[,colno])))  {break}
-      }
-    }
-  
-    #check categorical variable
-    for (column in 1:NCOL(fix_predictors))
-    {
-      if(class(fix_predictors[,column]) != "integer" & class(fix_predictors[,column]) != "numeric")
-      {
-        if(length(unique(fix_predictors[,column]))==2)
-        {
-          cat(paste("The binary variable '",colnames(fix_predictors)[column],"' will be recoded with ",unique(fix_predictors[,column])[1],"=0 and ",unique(fix_predictors[,column])[2],"=1 for the analysis\n",sep=""))
-            
-          recode=rep(0,NROW(fix_predictors))
-          recode[fix_predictors[,column]==unique(fix_predictors[,column])[2]]=1
-          fix_predictors[,column]=recode
-          IV_of_interest=fix_predictors[,colno]
-        } else if(length(unique(fix_predictors[,column]))>2)    {cat(paste("The categorical variable '",colnames(fix_predictors)[column],"' contains more than 2 levels, please code it into binarized dummy variables",sep=""))}
-      }      
-    }
-    
-    #check length of CT data and smoothing
-    n_vert=ncol(CT_data)
-    if(n_vert==20484) 
-    {    
-      template="fsaverage5"
-      load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/ROImap_fs5.rdata?raw=TRUE"))
-    }
-    else if (n_vert==81924) 
-    {
-      template="fsaverage6"
-      load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/ROImap_fs6.rdata?raw=TRUE"))
-    } 
-    else {stop("CT_data should only contain 20484 (fsaverage5) or 81924 (fsaverage6) columns")}
-
   #smoothing; to be implemented
   #if(missing("smooth"))
   #{
