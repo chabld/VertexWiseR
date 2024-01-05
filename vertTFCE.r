a29160d1a5d90cb7a7fd2127558f62a736b5e936
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-05 18:37:37 +0800

    Update vertTFCE.r

--- vertTFCE.r
+++ vertTFCE.r
@@ -17,13 +17,6 @@ TFCE.vertex_analysis=function(model,contrast, CT_data, nperm=100, tail=2, nthrea
     if(n_vert==20484)  {load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/edgelistfs5.rdata?raw=TRUE"),envir = globalenv())}
     else if (n_vert==81924) {load(file = url("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/data/edgelistfs6.rdata?raw=TRUE"),envir = globalenv())} 
     else {stop("CT_data should only contain 20484 (fsaverage5) or 81924 (fsaverage6) columns")}
-  
-  ##smoothing
-#  if(missing("smooth"))
-#  {
-#    if(n_vert==20484) {CT_data=smooth(CT_data, FWMH=10)}
-#    else if(n_vert==81924) {CT_data=smooth(CT_data, FWMH=5)}
-#  } else if(smooth>0) {CT_data=smooth(CT_data, FWMH=smooth)}
      
   ##unpermuted model
   model=data.matrix(model)
