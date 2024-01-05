5b3e78788f045f3c039fe543668d0035eeca941e
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-06 02:04:50 +0800

    Update vertwise.r

--- vertwise.r
+++ vertwise.r
@@ -11,25 +11,25 @@ vertex_analysis=function(all_predictors,IV_of_interest, random_effect, CT_data, 
   check.inputs(packages = "reticulate", CT_data = CT_data,all_predictors = all_predictors,IV_of_interest = IV_of_interest)
   
   ##smoothing
-  n_vert=ncol(CT_data)
+  nvert=NCOL(CT_data)
   if(missing("smooth_FWHM"))
   {
     if(n_vert==20484) 
     {
       cat("CT_data will be smoothed using the default 10mm FWHM kernel for fsaverage5 images\n")
-      CT_data=smooth(CT_data, FWHM=10)
+      CT_data=suppressMessages(smooth(CT_data, FWHM=10))
       cat("\nSmoothing completed")
     }
     else if(n_vert==81924) 
     {
       cat("CT_data will be smoothed using the default 5mm FWHM kernel for fsaverage6 images")
-      CT_data=smooth(CT_data, FWHM=5)
+      CT_data=suppressMessages(smooth(smooth(CT_data, FWHM=5))
       cat("\nSmoothing completed")
     }
   } else if(smooth>0) 
   {
     cat(paste("CT_data will be smoothed using a ", smooth,"mm FWHM kernel", sep=""))
-    CT_data=smooth(CT_data, FWHM=smooth_FWHM)
+    CT_data=suppressMessages(smooth(smooth(CT_data, FWHM=smooth_FWHM))
     cat("\nSmoothing completed")
   }
       
