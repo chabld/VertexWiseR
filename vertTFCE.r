93be127c1ba99a24d175c32719dd87018c8ab1df
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-08 23:57:24 +0800

    Update vertTFCE.r

--- vertTFCE.r
+++ vertTFCE.r
@@ -109,23 +109,27 @@ TFCE.vertex_analysis=function(model,contrast, CT_data, nperm=100, tail=2, nthrea
       }
     }
   
-  ##smoothing
+    ##smoothing
+    n_vert=NCOL(CT_data)
     if(missing("smooth_FWHM"))
     {
       if(n_vert==20484) 
       {
+        reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/smooth.py?raw=TRUE")
         cat("CT_data will be smoothed using the default 10mm FWHM kernel for fsaverage5 images\n")
-        CT_data=smooth(CT_data, FWHM=10)
+        CT_data=mesh_smooth(CT_data, FWHM=10)
       }
       else if(n_vert==81924) 
       {
+        reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/smooth.py?raw=TRUE")
         cat("CT_data will be smoothed using the default 5mm FWHM kernel for fsaverage6 images")
-        CT_data=smooth(CT_data, FWHM=5)
+        CT_data=mesh_smoothsmooth(CT_data, FWHM=5)
       }
     } else if(smooth>0) 
     {
+      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/smooth.py?raw=TRUE")
       cat(paste("CT_data will be smoothed using a ", smooth,"mm FWHM kernel", sep=""))
-      CT_data=smooth(CT_data, FWHM=smooth_FWHM)
+      CT_data=mesh_smooth(CT_data, FWHM=smooth_FWHM)
     }
       
   ##unpermuted model
