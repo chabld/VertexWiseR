c0f51ea1679b9adb6329c55a5e1220c265062639
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-14 13:47:38 +0800

    Update vertmixedTFCE.R

--- vertmixedTFCE.R
+++ vertmixedTFCE.R
@@ -150,7 +150,7 @@ TFCE.vertex_analysis.mixed=function(model,contrast, CT_data, random, nperm=100, 
   } else if(smooth_FWHM>0) 
   {
     reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/smooth.py?raw=TRUE")
-    cat(paste("CT_data will be smoothed using a ", smooth,"mm FWHM kernel", sep=""))
+    cat(paste("CT_data will be smoothed using a ", smooth_FWHM,"mm FWHM kernel", sep=""))
     CT_data=mesh_smooth(CT_data, FWHM=smooth_FWHM) 
   }
   CT_data[is.na(CT_data)]=0
