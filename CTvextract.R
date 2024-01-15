7a955180d9c293823aac05a85c5c9f716a6e0a6a
Charly Hugo Alexandre Billaud (Dr) <charly.billaud@staff.main.ntu.edu.sg>
2024-01-15 16:28:06 +0800

    Add CTvextract function

---
+++ CTvextract.R
@@ -0,0 +1,28 @@
+#' @title CTvextract
+#'
+#' @description Extracts whole-brain vertex-wise cortical thickness values for each subject in a Freesurfer output subjects directory, resamples the data to a common surface template, and stores it into a single matrix. This function requires the Freesurfer environment to be preset; and the 'freesurferformats' R package.
+#'
+#' @param sdirpath A string object containing the path to the Freesurfer output subjects directory
+#' @param template: A string object containing the name of surface template (currently available: 'fsaverage5', 'fsaverage6')
+#'
+#' @return A matrix object that can be used independently by VertexWiseR to compute statistical analyses. Each row corresponds to a subject's left and right hemisphere vertex-wise cortical thickness values
+#' @examples
+#' CTvextract('myfreesurfer_output_path/subjects_directory/', template='fsaverage5') 
+#' @export
+
+CTvextract=function(sdirpath, template='fsaverage5') 
+{ 
+#finds specifically subject folders in the directory (checks if a surf folder is present) and stores their ordered IDs in a list  
+system(paste0("export SUBJECTS_DIR=", sdirpath))
+system("find $SUBJECTS_DIR -maxdepth 1 -type d -exec test -e '{}/surf' \\; -exec basename {} > ./sublist.txt \\; \n sort -n ./sublist.txt -o ./sublist.txt")
+  
+#Calls Freesurfer to extract vertex-wise thickness data from the sample and resample it to the fsaverage5 common-space surface; and concatenate it into mgh files
+system(paste0("ln -s $FREESURFER_HOME/subjects/", template, " -t $SUBJECTS_DIR \n
+       mris_preproc --f ./sublist.txt --target fsaverage5 --hemi lh --meas thickness --out lh.mgh \n 
+       mris_preproc --f ./sublist.txt --target fsaverage5 --hemi rh --meas thickness --out rh.mgh"))
+
+#Reads mgh files to stores and assign the thickness values to each subject in a matrix object usable by VertexWiseR
+library(freesurferformats) 
+CT=t(rbind(drop(read.fs.mgh("lh.mgh")),drop(read.fs.mgh("rh.mgh"))))
+saveRDS(CT, file="CTv.rds")
+}
