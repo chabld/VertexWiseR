7b2daa8ca00ed046efc6f243ccd91beeb7f70935
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-18 16:33:30 +0800

    Update otherfunc.r

--- otherfunc.r
+++ otherfunc.r
@@ -249,7 +249,8 @@ plotCT=function(data, filename,title="",surface="inflated",cmap,fs_path, limits,
     ##hippocampal plots
       #import python libraries
       reticulate::source_python("https://github.com/CogBrainHealthLab/hippunfold_toolbox/blob/main/hippunfold_toolbox/plotting.py?raw=TRUE")
-    
+
+      data[data==0]=NA
       CTplot=surfplot_canonical_foldunfold(cbind(data[1:7262],data[7263:14524]),labels="hipp",color_bar=colorbar,
                                            nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),return_plotter=T,
                                            cmap=cmap,color_range=limits,label_text=list('left'=list(title)))
