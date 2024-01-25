9824e0d06efc6658b769d5a233652d5d479d5f17
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-25 15:47:16 +0800

    Update otherfunc.R

--- R/otherfunc.R
+++ R/otherfunc.R
@@ -209,17 +209,18 @@ fs6_to_fs5=function(surf_data)
 ############################################################################################################################
 ############################################################################################################################
 ##Cortical surface/hippocampal plots
-plotCT=function(surf_data, filename,title="",surface="inflated",cmap,fs_path, limits, colorbar=T)
+##input surf_data can be a matrix with multiple rows, for multiple plots in a single .png file
+plotCT=function(surf_data, filename,title="",surface="inflated",cmap, limits, colorbar=T)
 {
+  #format title for single row
+  if(is.null(nrow(surf_data)))  {title=list('left'=list(title))}
+  
   #check length of vector
   n_vert=length(surf_data)
-  if(n_vert==20484) {template="fsaverage5"}
-  else if (n_vert==81924) {template="fsaverage6"} 
-  else if (n_vert!=14524) {stop("surf_data vector should only contain 20484 (fsaverage5), 81924 (fsaverage6) or 14524 (hippocampal vertices) columns")}
+  if(n_vert%%20484==0) {template="fsaverage5"}
+  else if (n_vert%%81924==0) {template="fsaverage6"} 
+  else if (n_vert%%14524!=0) {stop("surf_data vector should only contain 20484 (fsaverage5), 81924 (fsaverage6) or 14524 (hippocampal vertices) columns")}
   
-  #legacy input message
-  if(!missing("fs_path")){cat("The fs_path parameter and the fsaverage5 files are no longer needed in the updated plotCT function\n")}
-
   #setting color maps
   if(missing("cmap"))
   {
@@ -232,31 +233,32 @@ plotCT=function(surf_data, filename,title="",surface="inflated",cmap,fs_path, li
   if(missing("limits")) {limits=range(surf_data,na.rm = T)}
   limits=reticulate::tuple(limits[1],limits[2])
   
-  if(n_vert!=14524)
+  if(n_vert%%14524!=0)
   {
     ##cortical surface fplots
       #import python libraries
-      brainstat.datasets=reticulate::import("brainstat.datasets")  
+      brainstat.surf_datasets=reticulate::import("brainstat.surf_datasets")  
       brainspace.plotting=reticulate::import("brainspace.plotting")  
       
       #loading fsaverage surface
-      left=brainstat.datasets$fetch_template_surface(template, join=F, layer=surface)[1]
-      right=brainstat.datasets$fetch_template_surface(template, join=F, layer=surface)[2]
+      left=brainstat.surf_datasets$fetch_template_surface(template, join=F, layer=surface)[1]
+      right=brainstat.surf_datasets$fetch_template_surface(template, join=F, layer=surface)[2]
       
       CTplot=brainspace.plotting$plot_hemispheres(left[[1]], right[[1]],  array_name=reticulate::np_array(surf_data),cmap=cmap, 
                                                   size=reticulate::tuple(as.integer(c(1920,400))),nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),
                                                   return_plotter=T,background=reticulate::tuple(as.integer(c(1,1,1))),zoom=1.25,color_range=limits,
-                                                  label_text=list('left'=list(title)),interactive=F, color_bar=colorbar,  transparent_bg=FALSE)  
+                                                  label_text=title,interactive=F, color_bar=colorbar,  transparent_bg=FALSE)  
   } else
   {
     ##hippocampal plots
       #import python libraries
-      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/hipp_plot.py?raw=TRUE")
-
-      surf_data[surf_data==0]=NA
-      CTplot=surfplot_canonical_foldunfold(cbind(surf_data[1:7262],surf_data[7263:14524]),color_bar=colorbar,share="row",
-                                           nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),
-                                           cmap=cmap,color_range=limits,label_text=list('left'=list(title)))
+      reticulate::source_python("hipp_plot.py")
+      
+      #formatting surf_data into two hemispheres because in put has to be a 7262 x 2 x N array
+      if(is.null(nrow(surf_data)))  {surf_data=cbind(surf_data[1:7262],surf_data[7263:14524])} 
+      else  {surf_data=array(cbind(surf_data[,1:7262],surf_data[,7263:14524]),c(7262,2,nrow(surf_data)))}
+      
+      CTplot=surfplot_canonical_foldunfold(surf_data,color_bar=colorbar,share="row",nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),cmap=cmap,color_range=limits,label_text=title)
   }
   #output plot as a .png image
   CTplot$screenshot(filename=filename,transparent_bg = F)
