37728e0253f1ea5cd7060247a36a446b610c23b2
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-25 19:01:22 +0800

    Update otherfunc.R

--- R/otherfunc.R
+++ R/otherfunc.R
@@ -210,20 +210,23 @@ fs6_to_fs5=function(surf_data)
 ############################################################################################################################
 ##Cortical surface/hippocampal plots
 ##input surf_data can be a matrix with multiple rows, for multiple plots in a single .png file
-plotCT=function(surf_data, filename,title="",surface="inflated",cmap, limits, colorbar=T)
+plotCT=function(surf_data, filename,title="",surface="inflated",cmap,fs_path, limits, colorbar=T)
 {
   #format title for single row
   if(is.null(nrow(surf_data))) 
-    {
+  {
     title=list('left'=list(title))
     rows=1
-    } else {rows=nrow(surf_data)}
+  } else {rows=nrow(surf_data)}
   
   #check length of vector
   n_vert=length(surf_data)
   if(n_vert%%20484==0) {template="fsaverage5"}
   else if (n_vert%%81924==0) {template="fsaverage6"} 
   else if (n_vert%%14524!=0) {stop("surf_data vector should only contain 20484 (fsaverage5), 81924 (fsaverage6) or 14524 (hippocampal vertices) columns")}
+  
+  #legacy input message
+  if(!missing("fs_path"))  {cat("The fs_path parameter and the fsaverage5 files are no longer needed in the updated plotCT function\n")}
   
   #setting color maps
   if(missing("cmap"))
@@ -240,31 +243,38 @@ plotCT=function(surf_data, filename,title="",surface="inflated",cmap, limits, co
   if(n_vert%%14524!=0)
   {
     ##cortical surface fplots
-      #import python libraries
-      brainstat.surf_datasets=reticulate::import("brainstat.surf_datasets")  
-      brainspace.plotting=reticulate::import("brainspace.plotting")  
-      
-      #loading fsaverage surface
-      left=brainstat.surf_datasets$fetch_template_surface(template, join=F, layer=surface)[1]
-      right=brainstat.surf_datasets$fetch_template_surface(template, join=F, layer=surface)[2]
-      
-      CTplot=brainspace.plotting$plot_hemispheres(left[[1]], right[[1]],  array_name=reticulate::np_array(surf_data),cmap=cmap, 
-                                                  size=reticulate::tuple(as.integer(c(1920,400*rows))),nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),
-                                                  return_plotter=T,background=reticulate::tuple(as.integer(c(1,1,1))),zoom=1.25,color_range=limits,
-                                                  label_text=title,interactive=F, color_bar=colorbar,  transparent_bg=FALSE) ##disabling interactive mode because this causes RStudio to hang
-  }
+    #import python libraries
+    brainstat.surf_datasets=reticulate::import("brainstat.surf_datasets")  
+    brainspace.plotting=reticulate::import("brainspace.plotting")  
+    
+    #loading fsaverage surface
+    left=brainstat.surf_datasets$fetch_template_surface(template, join=F, layer=surface)[1]
+    right=brainstat.surf_datasets$fetch_template_surface(template, join=F, layer=surface)[2]
+    
+    CTplot=brainspace.plotting$plot_hemispheres(left[[1]], right[[1]],  array_name=reticulate::np_array(surf_data),cmap=cmap, 
+                                                size=reticulate::tuple(as.integer(c(1920,rows*400))),nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),
+                                                return_plotter=T,background=reticulate::tuple(as.integer(c(1,1,1))),zoom=1.25,color_range=limits,
+                                                label_text=title,interactive=F, color_bar=colorbar,  transparent_bg=FALSE)  ##disabling interactive mode because this causes RStudio to hang
   } else
   {
     ##hippocampal plots
-      #import python libraries
-      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/hipp_plot.py?raw=TRUE")
-      
-      #reshaping data into a 7262 x 2 x N array
-      if(is.null(nrow(surf_data)))  {surf_data=cbind(surf_data[1:7262],surf_data[7263:14524])} ##if N=1
-      else  {surf_data=array(cbind(surf_data[,1:7262],surf_data[,7263:14524]),c(7262,2,nrow(surf_data)))} ##if N>1
-      
-      CTplot=surfplot_canonical_foldunfold(surf_data,color_bar=colorbar,share="row",nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),size=reticulate::tuple(as.integer(c(1920,400*rows))),
-                                           cmap=cmap,color_range=limits,label_text=title, return_plotter=T,interactive=F) ##disabling interactive mode because this causes RStudio to hang
+    #import python libraries
+    reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/hipp_plot.py?raw=TRUE")
+    
+    #reshaping surf_data into a 7262 x 2 x N array
+    if(is.null(nrow(surf_data)))  {surf_data=cbind(surf_data[1:7262],surf_data[7263:14524])} #if N=1
+    else  
+      {
+        surf_data.3d=array(NA,c(7262,2,nrow(surf_data))) #if N>1
+        for (row in 1:nrow(surf_data))
+        {
+          surf_data.3d[,,row]=cbind(surf_data[row,1:7262],surf_data[row,7263:14524])
+        }
+        surf_data=surf_data.3d
+      }
+    
+    CTplot=surfplot_canonical_foldunfold(surf_data,color_bar=colorbar,share="row",nan_color=reticulate::tuple(0.7, 0.7, 0.7, 1),size=as.integer(c(350,300)),
+                                         cmap=cmap,color_range=limits,label_text=title, return_plotter=T,interactive=F) ##disabling interactive mode because this causes RStudio to hang
   }
   #output plot as a .png image
   CTplot$screenshot(filename=filename,transparent_bg = F)
