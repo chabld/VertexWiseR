1cad3a8c3ceb5eacc150ca07c9b54947e949812e
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-02-05 23:15:34 +0800

    Update vertTFCE.R

--- R/vertTFCE.R
+++ R/vertTFCE.R
@@ -9,7 +9,7 @@ TFCE.vertex_analysis=function(model,contrast, surf_data, nperm=100, tail=2, nthr
 {
   if(class(contrast)=="integer") {contrast=as.numeric(contrast)}
   ##load other vertex-wise functions
-  source("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/otherfunc.r?raw=TRUE")
+  source("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/R/otherfunc.r?raw=TRUE")
   
   ##checks
     #check if nrow is consistent for model and surf_data
@@ -112,27 +112,23 @@ TFCE.vertex_analysis=function(model,contrast, surf_data, nperm=100, tail=2, nthr
   {
     if(n_vert==20484) 
     {
-      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
       cat("surf_data will be smoothed using the default 10mm FWHM kernel for fsaverage5 images\n")
-      surf_data=mesh_smooth(surf_data, FWHM=10)
+      surf_data=smooth(surf_data, FWHM=10)
     }
     else if(n_vert==81924) 
     {
-      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
       cat("surf_data will be smoothed using the default 5mm FWHM kernel for fsaverage6 images\n")
-      surf_data=mesh_smooth(surf_data, FWHM=5)
+      surf_data=smooth(surf_data, FWHM=5)
     }
     else if(n_vert==14524) 
     {
-      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
       cat("surf_data will be smoothed using the default 5mm FWHM kernel for hippocampal maps\n")
-      surf_data=mesh_smooth(surf_data, FWHM=5)
+      surf_data=smooth(surf_data, FWHM=5)
     }
   } else if(smooth_FWHM>0) 
   {
-    reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
     cat(paste("surf_data will be smoothed using a ", smooth_FWHM,"mm FWHM kernel\n", sep=""))
-    surf_data=mesh_smooth(surf_data, FWHM=smooth_FWHM)
+    surf_data=smooth(surf_data, FWHM=smooth_FWHM)
   }
   
   ##unpermuted model
