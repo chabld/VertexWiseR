4a6df26446a115761cdd16336cd8d04ce5635fb8
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-02-05 23:14:34 +0800

    Update vertmixedTFCE.R

--- vertmixedTFCE.R
+++ vertmixedTFCE.R
@@ -150,27 +150,23 @@ TFCE.vertex_analysis.mixed=function(model,contrast, CT_data, random, nperm=100, 
     {
       if(n_vert==20484) 
       {
-        reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
         cat("CT_data will be smoothed using the default 10mm FWHM kernel for fsaverage5 images\n")
-        CT_data=mesh_smooth(CT_data, FWHM=10)
+        CT_data=smooth(CT_data, FWHM=10)
       }
       else if(n_vert==81924) 
       {
-        reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
         cat("CT_data will be smoothed using the default 5mm FWHM kernel for fsaverage6 images\n")
-        CT_data=mesh_smooth(CT_data, FWHM=5)
+        CT_data=smooth(CT_data, FWHM=5)
       }
       else if(n_vert==14524) 
       {
-        reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
         cat("CT_data will be smoothed using the default 5mm FWHM kernel for hippocampal maps\n")
-        CT_data=mesh_smooth(CT_data, FWHM=5)
+        CT_data=smooth(CT_data, FWHM=5)
       }
     } else if(smooth_FWHM>0) 
     {
-      reticulate::source_python("https://github.com/CogBrainHealthLab/VertexWiseR/blob/main/python/smooth.py?raw=TRUE")
       cat(paste("CT_data will be smoothed using a ", smooth_FWHM,"mm FWHM kernel\n", sep=""))
-      CT_data=mesh_smooth(CT_data, FWHM=smooth_FWHM)
+      CT_data=smooth(CT_data, FWHM=smooth_FWHM)
     }
     CT_data[is.na(CT_data)]=0
   
