c6ef49c2f08aadd36ccab95dbc24281fc769342a
Charly Hugo Alexandre Billaud (Dr) <charly.billaud@staff.main.ntu.edu.sg>
2024-02-14 13:57:46 +0800

    Improved Rdoc

--- R/SURFvextract.R
+++ R/SURFvextract.R
@@ -1,15 +1,15 @@
 #' @title SURFvextract
 #'
-#' @description Extracts whole-brain vertex-wise cortical thickness values for each subject in a Freesurfer output subjects directory, resamples the data to a common surface template, and stores it into a single matrix. This function requires the Freesurfer environment to be preset; and the 'freesurferformats' R package.
-#' @details The function runs system shell commands that will produce in the set subjects directory: 1) a sorted list of subjects "sublist.txt"; 2) A link file to the selected surface fsaverage template. 3) left and right hemisphere .mgh maps outputted by FreeSurfer's mris_preproc.
+#' @description Extracts whole-brain vertex-wise surface-based measures for each subject in a Freesurfer output subjects directory, resamples the data to a common surface template, and stores it into a single matrix. This function requires the Freesurfer environment to be preset.
+#' @details The function runs system shell commands that will produce in the set subjects directory: 1) a sorted list of subjects "sublist.txt"; 2) a link file to the selected surface fsaverage template. 3) left and right hemisphere .mgh maps outputted by FreeSurfer's mris_preproc.
 #'
-#' @param sdirpath A string object containing the path to the Freesurfer output subjects directory.
+#' @param sdirpath A string object containing the path to the Freesurfer subjects directory.
 #' @param filename A string object containing the desired name of the output RDS file.
 #' @param template A string object containing the name of surface template (available: 'fsaverage5', 'fsaverage6'). Default is fsaverage5.
 #' @param measure A string object containing the name of the measure of interest. Options are thickness, curv, sulc, area. Default is thickness.
-#' @param subj_ID A logical object stating whether to include subject IDs (T) in the object or not (F). Default is TRUE.
+#' @param subj_ID A logical object stating whether to include subject IDs (folder names in the subjects directory) as a first column to the output matrix. Default is TRUE.
 #'
-#' @returns A .rds file containing a matrix object. The matrix can be used independently by VertexWiseR to compute statistical analyses. Each row corresponds to a subject's left and right hemisphere vertex-wise cortical thickness values
+#' @returns A .rds file containing a matrix object. The matrix can be used independently by VertexWiseR to compute statistical analyses. Each row corresponds to a subject's left and right hemisphere vertex-wise values
 #' @examples
 #' SURFvextract(sdirpath = "myfreesurfer_output_path/subjects_directory/", filename = "CTv", template="fsaverage5", measure = "curv") 
 #' @importFrom freesurferformats read.fs.mgh
